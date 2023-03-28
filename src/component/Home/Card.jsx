d0ea2413daec09a08a082ae28c4960a27bd99584
indranilmondal901 <indranilmondal901@gmail.com>
2023-03-28 14:17:57 +0530

    view btn logic left

--- src/component/Home/Card.jsx
+++ src/component/Home/Card.jsx
@@ -5,22 +5,18 @@ const Card = () => {
   useEffect(() => {
     axios.get("https://tuner.onrender.com/")
       .then((res) => {
-        // console.log(res.data.data[0].video.vfile)
-        console.log(res.data.data)
-        // setItem(res.data.data[0].video.vfile)
+        // console.log(res.data.data)
         setItem(res.data.data)
       })
   }, [])
   return (
-    <div id="card-video-wrapper">
+    <div id="card-video-wrapper" style={{display:"block"}}>
       {item.map((res, i) => {
         return (
-          <>
             <div key={i} className="card-video" >
               <video src={res.video.vfile} height="200px" width="300px" controls></video>
               <p>{res.video.name} [{res.video.category}]</p>
             </div>
-          </>
         )
       })}
     </div>
